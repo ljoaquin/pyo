73b3d773df3e07573c58776581baf65547094cd3
ljoaquin <lj.ljoaquin@gmail.com>
2017-03-06 20:35:07 +0800

    win32 support cmake

--- make.py
+++ make.py
@@ -1,11 +1,16 @@
 import trivia
+import sys
+
+BUILD_PATH = "./build"
 
 def main():
     cmds = [
-        "rm -rf bin build",
-        "cmake -H. -Bbuild",
-        "make -C build/",
+        "rm -rf %s" % BUILD_PATH,
+        "cmake -H. -B%s" % BUILD_PATH,
+        "cmake --build %s" % BUILD_PATH,
     ]
+    if sys.platform == "win32":
+        cmds[1] = "cmake -H. -Bbuild -G \"Visual Studio 12 2013\""
     for cmd in cmds:
         r = trivia.run(cmd)
         if r != 0:
